526884209e55567a50fb9fdc3f5afc395020a76a
moaaztaha <moaaztaha7@gmail.com>
2021-09-26 15:52:10 +0200

    fixed

--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def setup():
-    print("Cloning repos....")
-    os.system("git clone https://github.com/moaaztaha/Arabic-Image-Captioning-Demo")
-    os.system("git clone https://github.com/aub-mind/arabert")
-    os.system("mv  -v /content/Arabic-Image-Captioning-Demo/* .")
-    os.system("pip install -r requirements.txt")
