5cb0d5b44f51ac9c93b1c21009bcbe12dd83f981
moaz <moaz@pop-os.localdomain>
2022-05-21 10:10:33 +0200

    new caption

--- caption.py
+++ caption.py
@@ -3,7 +3,14 @@ import torch.nn.functional as F
 import numpy as np
 import json
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 from PIL import Image
+
+
+import skimage.transform
+import matplotlib.cm as cm
+import arabic_reshaper
+from bidi.algorithm import get_display
 
 from dataset import build_vocab
 from utils import *
@@ -156,3 +163,203 @@ def caption_image(upload=None, img_path='test.png', beam_size=3, old=False):
     seq = complete_seqs[i]
 
     return arabert_prep.unpreprocess(" ".join([vocab.itos[i] for i in seq[1:-1]]))
+
+
+
+def caption_image_viz(img_path, beam_size=3, old=False):
+    
+    # transforms
+    tt = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+    ])
+
+
+    if old:
+            # old
+        vocab = build_vocab('old_ar_data.json')
+        checkpoint = load_checkpoint('BEST_checkpoint_flickr8k_ar_pretrained_finetune.pth.tar', cpu=True)
+    else:
+        # arabert
+        vocab = build_vocab('ar_data.json')
+        checkpoint = load_checkpoint('BEST_checkpoint_flickr8k_ar_arabert_pretrained_finetune.pth.tar', cpu=True)
+
+    addit_tokens = [vocab.stoi['<sos>'], vocab.stoi['<eos>'], vocab.stoi['<pad>']]
+    device = torch.device( 'cpu')
+
+
+    encoder = checkpoint['encoder'].to(device)
+    decoder = checkpoint['decoder'].to(device)
+
+    #def cap_image(encoder, decoder, image_path, vocab):
+    vocab_size = len(vocab)
+
+
+    img = Image.open(img_path).convert("RGB")
+    img = tt(img).unsqueeze(0) # transform and batch
+    image = img.to(device)
+
+    #encoder
+    encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
+    enc_image_size = encoder_out.size(1)
+    encoder_dim = encoder_out.size(3)
+
+    # Flatten encoding
+    encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
+    num_pixels = encoder_out.size(1)
+
+
+
+    k = beam_size 
+    # We'll treat the problem as having a batch size of k
+    encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
+
+    # Tensor to store top k previous words at each step; now they're just <start>
+    k_prev_words = torch.LongTensor([[vocab.stoi['<sos>']]] * k).to(device)  # (k, 1)
+
+    # Tensor to store top k sequences; now they're just <start>
+    seqs = k_prev_words  # (k, 1)
+
+    # Tensor to store top k sequences' scores; now they're just 0
+    top_k_scores = torch.zeros(k, 1).to(device)  # (k, 1)
+
+    # Tensor to store top k sequences' alphas; now they're just 1s
+    seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device)  # (k, 1, enc_image_size, enc_image_size)
+
+    # Lists to store completed sequences, their alphas and scores
+    complete_seqs = list()
+    complete_seqs_alpha = list()
+    complete_seqs_scores = list()
+
+    # Start decoding
+    step = 1
+    h, c = decoder.init_hidden_state(encoder_out)
+
+    # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
+    while True:
+
+        embeddings = decoder.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)
+
+        awe, alpha = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
+
+        alpha = alpha.view(-1, enc_image_size, enc_image_size)  # (s, enc_image_size, enc_image_size)
+
+        gate = decoder.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
+        awe = gate * awe
+
+        h, c = decoder.decode_step(torch.cat([embeddings, awe], dim=1), (h, c))  # (s, decoder_dim)
+
+        scores = decoder.fc(h)  # (s, vocab_size)
+        scores = F.log_softmax(scores, dim=1)
+
+        # Add
+        scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
+
+        # For the first step, all k points will have the same scores (since same k previous words, h, c)
+        if step == 1:
+            top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
+        else:
+            # Unroll and find top scores, and their unrolled indices
+            top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (s)
+
+        # Convert unrolled indices to actual indices of scores
+        prev_word_inds = top_k_words // vocab_size  # (s)
+        next_word_inds = top_k_words % vocab_size  # (s)
+        
+        # Add new words to sequences, alphas
+        seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
+        seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
+                               dim=1)  # (s, step+1, enc_image_size, enc_image_size)
+#         print(seqs[prev_word_inds], prev_word_inds)
+#         if step == 5:
+#             return seqs
+        # Which sequences are incomplete (didn't reach <end>)?
+        incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
+                           next_word != vocab.stoi['<eos>']]
+        complete_inds = list(set(range(len(next_word_inds))) - set(incomplete_inds))
+
+        # Set aside complete sequences
+        if len(complete_inds) > 0:
+            complete_seqs.extend(seqs[complete_inds].tolist())
+            complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
+            complete_seqs_scores.extend(top_k_scores[complete_inds])
+        k -= len(complete_inds)  # reduce beam length accordingly
+
+        # Proceed with incomplete sequences
+        if k == 0:
+            break
+        seqs = seqs[incomplete_inds]
+        seqs_alpha = seqs_alpha[incomplete_inds]
+        h = h[prev_word_inds[incomplete_inds]]
+        c = c[prev_word_inds[incomplete_inds]]
+        encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
+        top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
+        k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
+
+        # Break if things have been going on too long
+        if step > 50:
+            break
+        step += 1
+
+    i = complete_seqs_scores.index(max(complete_seqs_scores))
+    seq = complete_seqs[i]
+    alphas = complete_seqs_alpha[i]
+
+    print(complete_seqs_scores)
+    # print(seq)
+    all_caps = [" ".join([vocab.itos[i] for i in sent if i not in addit_tokens]) for sent in complete_seqs]
+    # all_b_caps = ""
+    # z = 1
+    # for cap in all_caps:
+    #     all_b_caps += str(z) + ". " + cap + " || <br> "
+    #     z += 1
+    # all_b_caps = [" || ".join(all_caps)][0]
+    return alphas, seq, all_caps
+
+
+def visualize_att(image_path, seq, alphas, smooth=True, old=False):
+    """
+    Visualizes caption with weights at every word.
+    Adapted from paper authors' repo: https://github.com/kelvinxu/arctic-captions/blob/master/alpha_visualization.ipynb
+    :param image_path: path to image that has been captioned
+    :param seq: caption
+    :param alphas: weights
+    :param rev_word_map: reverse word mapping, i.e. ix2word
+    :param smooth: smooth weights?
+    """
+    image = Image.open(image_path)
+    image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
+
+    if old:
+            # old
+        vocab = build_vocab('old_ar_data.json')
+    else:
+        # arabert
+        vocab = build_vocab('ar_data.json')
+
+    addit_tokens = [vocab.stoi['<sos>'], vocab.stoi['<eos>'], vocab.stoi['<pad>']]
+    
+    words = [vocab.itos[i] for i in seq if i not in addit_tokens]
+
+    plt.figure(figsize=(30, 15)) 
+    for t in range(len(words)):
+        if t > 50:
+          break
+        plt.subplot(np.ceil(len(words) / 5.), 5, t + 1)
+        plt.text(0, 1, '%s' % (get_display( arabic_reshaper.reshape(words[t]))), color='black', backgroundcolor='white', fontsize=12)
+        plt.imshow(image)
+        current_alpha = alphas[t]
+        if smooth:
+            alpha = skimage.transform.pyramid_expand(current_alpha, upscale=24, sigma=8)
+        else:
+            alpha = skimage.transform.resize(current_alpha, [14 * 24, 14 * 24])
+        # if t == 0:
+        #     plt.imshow(alpha, alpha=0)
+        # else:
+        plt.imshow(alpha, alpha=0.7)
+        plt.set_cmap(cm.Greys_r)
+        plt.axis('off')
+    plt.show()
